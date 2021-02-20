ac9021097a9f7d9d70be2f793d8f57e849fff5f6
Jason Huff <jsnhff@gmail.com>
2021-02-19 17:35:33 -0800

    updating config to not minimize scss for now and tuning html and css layout

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,12 +1,12 @@
 // tailwind.config.js
 
 module.exports = {
-  purge: [
-    './_includes/**/*.html',
-    './_layouts/**/*.html',
-    './_posts/*.md',
-    './*.html',
-  ],
+//  purge: [
+//    './_includes/**/*.html',
+//    './_layouts/**/*.html',
+//    './_posts/*.md',
+//    './*.html',
+//  ],
 
   darkMode: false,
 
